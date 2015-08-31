43160ded5c2e77e95c650a4e549d6d005ad0ad9c
Tom Lin <tao.lin3@hp.com>
2015-08-31 08:36:51 +0800

    Display a GET request param.

--- src/main/java/org/spring/todo/controller/TodoController.java
+++ src/main/java/org/spring/todo/controller/TodoController.java
@@ -1,15 +1,24 @@
 package org.spring.todo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String index() {
+    public String index(@RequestParam(value = "version", required = false, defaultValue = "0.1") String version, Model model) {
+        model.addAttribute("version", version);
+        return "list";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String add() {
+        System.out.println("Got a post request!");
         return "list";
     }
 }
--- src/main/webapp/WEB-INF/views/list.jsp
+++ src/main/webapp/WEB-INF/views/list.jsp
@@ -1,9 +1,18 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <html>
 <head>
     <title>Spring Todo</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <h1>Hello World! ${version}</h1>
+    <form method="post" action="/spring-todo/todo">
+        <label>
+            <input type="text" name="description">
+        </label>
+        <input type="submit" value="Add">
+    </form>
 </body>
 </html>
